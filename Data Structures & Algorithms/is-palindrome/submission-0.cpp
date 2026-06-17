f91b1eeeb56e4529bcee43f3307aff31f7a3094c
Shraddha Bhatt <techsince2025@gmail.com>
2026-06-18 01:59:27 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        string cleaned = "";
+
+        for(char c : s){
+            if(isalnum(c)){
+                cleaned.push_back(tolower(c));
+            }
+        }
+
+        string rev = cleaned;
+        reverse(rev.begin(), rev.end());
+
+        return cleaned == rev;
+    }
+};
